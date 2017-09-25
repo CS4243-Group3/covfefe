26e06d86c71ec7c79fb6174adfc2dcbabad8b73f
Louis Lai <hoangdung2506@gmail.com>
2017-09-25 12:12:38 +0800

    can at least track a pendulum. iterative lk is buggy

--- lucaskanade.py
+++ lucaskanade.py
@@ -15,18 +15,16 @@ def track_features(old_gray, new_gray, features):
     # Select good points
     # good_new = p1[st==1]
     # good_old = features[st==1]
-    # print(p1)
+    print(p1)
 
     return (features, p1)
 
 def track_features(old_gray, new_gray, features):
-    old_gray = cv2.GaussianBlur(old_gray, (3, 3), 1).T
-    new_gray = cv2.GaussianBlur(new_gray, (3, 3), 1).T
-    # old_gray = old_gray.T
-    # new_gray = new_gray.T
+    # old_gray = cv2.GaussianBlur(old_gray, (3, 3), 1).T
+    # new_gray = cv2.GaussianBlur(new_gray, (3, 3), 1).T
 
     # Calculative partial derivatives wrt x, y & time
-    kernel = np.array([[-1, 1]])
+    kernel = np.array([[-1, 0, 1]]) / 2.
     I_x = convolve2d(old_gray, kernel, 'same')
     I_y = convolve2d(old_gray, kernel.T, 'same')
     I_t = old_gray - new_gray
@@ -34,36 +32,63 @@ def track_features(old_gray, new_gray, features):
     I_xx = I_x * I_x
     I_xy = I_x * I_y
     I_yy = I_y * I_y
-    I_tx = I_t * I_x
-    I_ty = I_t * I_y
 
     window_size = 15
+    w = window_size // 2
+    num_iterations = 1
+
     ones = np.ones((window_size, window_size))
 
     weighted_I_xx = convolve2d(I_xx, ones, 'same')
     weighted_I_xy = convolve2d(I_xy, ones, 'same')
     weighted_I_yy = convolve2d(I_yy, ones, 'same')
 
-    weighted_I_tx = convolve2d(I_tx, ones, 'same')
-    weighted_I_ty = convolve2d(I_ty, ones, 'same')
-
     good_old = []
     new_features = []
+
     for feature in features:
         idx = feature[0].astype(np.int)
-        if idx[0] > old_gray.shape[0] or idx[1] > old_gray.shape[1]:
-            print(idx)
+
+        left = max(0, idx[0] - w)
+        right = min(old_gray.shape[0], idx[0] + w)
+        top = max(0, idx[1] - w)
+        bottom = min(old_gray.shape[1], idx[1] + w)
+
+        i_x = I_x[top:bottom+1, left:right+1]
+        i_y = I_y[top:bottom+1, left:right+1]
+        d = np.zeros(2)
+        err = False
+
+
+        for _ in range(num_iterations):
+
+            try:
+                xy = (idx + d).astype(np.int)
+                Z = np.array([[weighted_I_xx[xy[1], xy[0]], weighted_I_xy[xy[1], xy[0]]],
+                              [weighted_I_xy[xy[1], xy[0]], weighted_I_yy[xy[1], xy[0]]]])
+
+                dx = d[0].astype(np.int)
+                dy = d[1].astype(np.int)
+                i1 = old_gray[top:bottom+1, left:right+1]
+                i2 = new_gray[top+dy:bottom+1+dy, left+dx:right+1+dx]
+
+                i_t = i1 - i2
+                i_tx = i_t * i_x
+                i_ty = i_t * i_y
+
+                b = np.array([np.sum(i_tx.flatten()), np.sum(i_ty.flatten())])
+                d += np.linalg.lstsq(Z, b)[0]
+                # print(d, idx + d)
+            except:
+                err = True
+                break
+
+        if err:
             continue
 
-        Z = np.array([[weighted_I_xx[idx[0], idx[1]], weighted_I_xy[idx[0], idx[1]]],
-                      [weighted_I_xy[idx[0], idx[1]], weighted_I_yy[idx[0], idx[1]]]])
-        b = np.array([weighted_I_tx[idx[0], idx[1]], weighted_I_ty[idx[0], idx[1]]])
-        d = np.linalg.lstsq(Z, b)[0]
         good_old.append(idx)
         new_features.append(feature[0] + d)
     new_features = np.array(new_features).astype(np.float32)
-    # print(new_features)
+    #print(new_features)
     return (np.array(good_old), new_features)
-
-
 
