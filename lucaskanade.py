44f708a13f240ba339b47e71e94085f76ac32eda
Louis Lai <hoangdung2506@gmail.com>
2017-09-26 16:24:21 +0800

    more attempt

--- lucaskanade.py
+++ lucaskanade.py
@@ -28,14 +28,14 @@ def track_features(old_gray, new_gray, features):
 
     num_pyramid = 1
 
-    window_size = 15
+    window_size = 13
     w = window_size // 2
     epsilon = 0.3
     num_iterations = 1
 
     pyramid = build_pyramid(old_gray, new_gray, num_pyramid, window_size)
     #guassian_weights = np.matmul(gaussian(window_size, 1)[:, None], gaussian(window_size, 1)[None, :])
-    guassian_weights = np.ones((window_size, window_size))
+    guassian_weights = 1.
 
     good_old = []
     new_features = []
@@ -74,8 +74,8 @@ def track_features(old_gray, new_gray, features):
             top = idx[1] - w
             bottom = idx[1] + w
 
-            y_range = np.arange(top, bottom+1, 1).astype(np.int)
-            x_range = np.arange(left, right+1, 1).astype(np.int)
+            y_range = np.arange(top, bottom+1, 1)
+            x_range = np.arange(left, right+1, 1)
 
             i_x = f_I_x(y_range, x_range, grid=True)
             i_y = f_I_y(y_range, x_range, grid=True)
@@ -99,8 +99,8 @@ def track_features(old_gray, new_gray, features):
                     dx = d[0]
                     dy = d[1]
 
-                    new_gray_y_range = (np.arange(top, bottom + 1, 1) + dy + gy).astype(np.int)
-                    new_gray_x_range = (np.arange(left, right + 1, 1) + dx + gx).astype(np.int)
+                    new_gray_y_range = np.arange(top, bottom + 1, 1) + dy + gy
+                    new_gray_x_range = np.arange(left, right + 1, 1) + dx + gx
                     i2 = f_new(new_gray_y_range, new_gray_x_range, grid=True)
                     i1 = f_old(y_range, x_range, grid=True)
 
@@ -112,7 +112,7 @@ def track_features(old_gray, new_gray, features):
                     print('b', b)
                     eta = np.linalg.lstsq(Z, b)[0]
                     eta_norm = np.linalg.norm(eta)
-                    print('eta', eta_norm)
+                    print('eta', eta_norm, eta, d + eta)
                     d += eta
                 except:
                     err = True
@@ -135,9 +135,9 @@ def build_pyramid(old_gray, new_gray, num_pyramid, window_size):
     for i in range(1, num_pyramid):
         old, new, _ = pyramid[i-1]
         old = cv2.GaussianBlur(old, (3, 3), 1.5)
-        old = cv2.resize(old, (old.shape[0] // 2, old.shape[1] // 2))
+        old = cv2.resize(old, (old.shape[1] // 2, old.shape[0] // 2))
         new = cv2.GaussianBlur(new, (3, 3), 1.5)
-        new = cv2.resize(new, (new.shape[0] // 2, new.shape[1] // 2))
+        new = cv2.resize(new, (new.shape[1] // 2, new.shape[0] // 2))
         pyramid.append((old, new, build_derivatives(old, new, window_size)))
     return pyramid
 
@@ -153,25 +153,15 @@ def build_derivatives(old_gray, new_gray, window_size):
     I_xy = I_x * I_y
     I_yy = I_y * I_y
 
-    if BIN:
-        f_old = lambda y_range, x_range, grid: old_gray[y_range, x_range]
-        f_new = lambda y_range, x_range, grid:  new_gray[y_range, x_range]
-        f_I_xx = lambda y_range, x_range, grid:  I_xx[y_range, x_range]
-        f_I_xy = lambda y_range, x_range, grid:  I_xy[y_range, x_range]
-        f_I_yy = lambda y_range, x_range, grid:  I_yy[y_range, x_range]
-        f_I_x = lambda y_range, x_range, grid:  I_x[y_range, x_range]
-        f_I_y = lambda y_range, x_range, grid:  I_y[y_range, x_range]
-
-    else:
-        y_coords = np.arange(old_gray.shape[0])
-        x_coords = np.arange(old_gray.shape[1])
-        f_old = RectBivariateSpline(y_coords, x_coords, old_gray)
-        f_new = RectBivariateSpline(y_coords, x_coords, new_gray)
-        f_I_xx = RectBivariateSpline(y_coords, x_coords, I_xx)
-        f_I_xy = RectBivariateSpline(y_coords, x_coords, I_xy)
-        f_I_yy = RectBivariateSpline(y_coords, x_coords, I_yy)
-        f_I_x = RectBivariateSpline(y_coords, x_coords, I_x)
-        f_I_y = RectBivariateSpline(y_coords, x_coords, I_y)
+    y_coords = np.arange(old_gray.shape[0])
+    x_coords = np.arange(old_gray.shape[1])
+    f_old = get_range_values(y_coords, x_coords, old_gray, BIN)
+    f_new = get_range_values(y_coords, x_coords, new_gray, BIN)
+    f_I_xx = get_range_values(y_coords, x_coords, I_xx, BIN)
+    f_I_xy = get_range_values(y_coords, x_coords, I_xy, BIN)
+    f_I_yy = get_range_values(y_coords, x_coords, I_yy, BIN)
+    f_I_x = get_range_values(y_coords, x_coords, I_x, BIN)
+    f_I_y = get_range_values(y_coords, x_coords, I_y, BIN)
 
     return {
         'f_old': f_old,
@@ -182,3 +172,13 @@ def build_derivatives(old_gray, new_gray, window_size):
         'f_I_xy': f_I_xy,
         'f_I_yy': f_I_yy
     }
+
+def get_range_values(y_coords, x_coords, values, BIN):
+    if BIN:
+        def f(y_range, x_range, grid):
+            y_range = y_range.astype(np.int)
+            x_range = x_range.astype(np.int)
+            return values[y_range[0]:y_range[-1]+1, x_range[0]:x_range[-1]+1]
+        return f
+    else:
+        return RectBivariateSpline(y_coords, x_coords, values)
